bd7adf0d7583d3e0590226a9b2cf958b0b533bb8
Mohanad Ali <mohanad.ali@fmfyemen.com>
2026-04-29 00:35:03 +0300

    Fix Registerables line controller registration

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,10 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
 
+import { Chart as ChartJS, registerables } from 'chart.js';
+
+ChartJS.register(...registerables);
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
