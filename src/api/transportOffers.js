81bcd18b334050364368dcba914fe4527bea7a69
MohanadAli <102630539+fmfmohanadali@users.noreply.github.com>
2026-05-29 04:18:48 +0300

    Add transportOffersApi for creating transport offers

--- src/api/transportOffers.js
+++ src/api/transportOffers.js
@@ -1,1 +1,14 @@
+import { api } from './client';
 
+export const transportOffersApi = {
+  create: async ({ requestId, carrierCompany, price, estimatedPickupAt, note }) => {
+    const { data } = await api.post('/transport-offers/', {
+      request: requestId,
+      carrier_company: Number(carrierCompany),
+      price,
+      estimated_pickup_at: estimatedPickupAt || null,
+      note: note || '',
+    });
+    return data;
+  },
+};
