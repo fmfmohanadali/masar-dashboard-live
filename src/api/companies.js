3cd2f34880900a3a05fff91e7cd24c15aedd9abf
MohanadAli <102630539+fmfmohanadali@users.noreply.github.com>
2026-05-29 04:19:39 +0300

    Add companies API with list method

--- src/api/companies.js
+++ src/api/companies.js
@@ -1,1 +1,8 @@
+import { api, normalizeList } from './client';
 
+export const companiesApi = {
+  list: async () => {
+    const { data } = await api.get('/companies/');
+    return normalizeList(data);
+  },
+};
